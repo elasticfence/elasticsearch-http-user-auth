e3ea6893fba4800539756fb8c5c2b53f543ecf83
Tom Search <tomsearch.tech@gmail.com>
2016-01-31 23:53:45 +0900

    change logger name from ElasticfenceLogger to EFLogger

--- src/main/java/org/elasticsearch/plugin/elasticfence/ElasticfencePlugin.java
+++ src/main/java/org/elasticsearch/plugin/elasticfence/ElasticfencePlugin.java
@@ -3,7 +3,7 @@ package org.elasticsearch.plugin.elasticfence;
 import java.util.Arrays;
 
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.plugin.elasticfence.logger.ElasticfenceLogger;
+import org.elasticsearch.plugin.elasticfence.logger.EFLogger;
 import org.elasticsearch.rest.RestModule;
 import org.elasticsearch.plugins.Plugin;
 
@@ -13,7 +13,7 @@ public class ElasticfencePlugin extends Plugin {
 
     	public ElasticfencePlugin(Settings settings){	
         	this.settings = settings;
-        	ElasticfenceLogger.info("loading elasticfence plugin...");
+        	EFLogger.info("loading elasticfence plugin...");
     	}
 
         @Override
@@ -30,10 +30,10 @@ public class ElasticfencePlugin extends Plugin {
 //    public Collection<Class<? extends Module>> modules() {
     public void onModule(RestModule module) {
     	String isPluginDisabled = settings.get("elasticfence.disabled");
-    	ElasticfenceLogger.info("elasticfence.disabled: " + isPluginDisabled);
+    	EFLogger.info("elasticfence.disabled: " + isPluginDisabled);
         
     	if (isPluginDisabled != null && isPluginDisabled.toLowerCase().equals("true")) {
-            ElasticfenceLogger.warn("Elasticfence plugin is disabled");
+            EFLogger.warn("Elasticfence plugin is disabled");
     	} else {
         	String rootPassword = settings.get("elasticfence.root.password");
         	if (rootPassword != null && !rootPassword.equals("")) {
@@ -46,15 +46,15 @@ public class ElasticfencePlugin extends Plugin {
 
         	if (whitelist != null ) {
         		IPAuthenticator.setWhitelist(whitelist);
-	            	ElasticfenceLogger.warn("elasticfence plugin IP whitelist enabled " + Arrays.toString(whitelist));
+	            	EFLogger.warn("elasticfence plugin IP whitelist enabled " + Arrays.toString(whitelist));
         	}
         	if (blacklist != null ) {
         		IPAuthenticator.setBlacklist(blacklist);
-	            	ElasticfenceLogger.warn("elasticfence plugin IP blacklist enabled " + Arrays.toString(blacklist));
+	            	EFLogger.warn("elasticfence plugin IP blacklist enabled " + Arrays.toString(blacklist));
         	}
     		
             module.addRestAction(AuthRestHandler.class);
-            ElasticfenceLogger.info("elasticfence plugin is enabled");
+            EFLogger.info("elasticfence plugin is enabled");
     	}
     }
 }
