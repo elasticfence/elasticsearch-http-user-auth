81dba941aaec8db88280fd3a2369eaddbeb3bade
Ryan Lutgen <ryanlutgen@outlook.com>
2016-12-09 22:19:45 -0600

    Update client requests that tell ES to refresh, need to verify this works

--- src/main/java/org/elasticsearch/plugin/elasticfence/data/UserDataBridge.java
+++ src/main/java/org/elasticsearch/plugin/elasticfence/data/UserDataBridge.java
@@ -16,7 +16,8 @@ import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
-import org.elasticsearch.client.Client;
+import org.elasticsearch.action.support.WriteRequest;
+import org.elasticsearch.client.node.NodeClient;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import org.elasticsearch.index.query.QueryBuilders;
@@ -33,9 +34,9 @@ public class UserDataBridge {
 	private static final String HTTP_USER_AUTH_INDEX = ".http_user_auth";
 	private static final String HTTP_USER_AUTH_TYPE = "user";
 	private static boolean isInitialized = false;
-	private Client client;
+	private NodeClient client;
 	
-	public UserDataBridge(Client client) {
+	public UserDataBridge(NodeClient client) {
 		this.client = client;
 		if (!isInitialized && !createIndexIfEmpty()) {
 			EFLogger.error("failed to create index: " + HTTP_USER_AUTH_INDEX);
@@ -88,9 +89,8 @@ public class UserDataBridge {
 	
 	/**
 	 * add permission to an user with specified indices
-	 * @param user
-	 * @param password
-	 * @param path
+	 * @param userName
+	 * @param indexName
 	 * @return
 	 */
 	public boolean addAuthIndex (String userName, String indexName) {
@@ -128,9 +128,8 @@ public class UserDataBridge {
 	
 	/**
 	 * update permission of an user with specified indices
-	 * @param user
-	 * @param password
-	 * @param path
+	 * @param userName
+	 * @param indexName
 	 * @return
 	 */
 	public boolean updateAuthIndex (String userName, String indexName) {
@@ -168,9 +167,9 @@ public class UserDataBridge {
 	
 	/**
 	 * add permission to an user with a specified index
-	 * @param user
+	 * @param userName
 	 * @param password
-	 * @param path
+	 * @param indexName
 	 * @return
 	 */
 	public boolean removeAuth (String userName, String password, String indexName) {
@@ -231,7 +230,7 @@ public class UserDataBridge {
 			                        .field("created", created)
 			                    .endObject()
 			                  )
-			        .setRefresh(true)
+					.setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE)
 			        .execute()
 			        .get();
 			reloadUserDataCache();
@@ -267,13 +266,13 @@ public class UserDataBridge {
 		DeleteResponse response = null;
 		try {
 			response = client.prepareDelete(HTTP_USER_AUTH_INDEX, HTTP_USER_AUTH_TYPE, userName)
-					.setRefresh(true)
+					.setRefreshPolicy(WriteRequest.RefreshPolicy.IMMEDIATE)
 			        .execute()
 			        .get();
 		} catch (InterruptedException | ExecutionException e) {
 			EFLogger.error("InterruptedException | ExecutionException", e);
 		}
-		if (response != null && response.isFound()) {
+		if (response != null && response.status().getStatus() == 200) {
 			reloadUserDataCache();
 			return true;
 		}
