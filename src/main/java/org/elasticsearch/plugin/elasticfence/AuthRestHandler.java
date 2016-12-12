06a5a14afc8ffdf173bc34237d67e236a8f569b5
Ryan Lutgen <ryanlutgen@outlook.com>
2016-12-12 12:24:55 -0600

    Move rest handling into prepareRequest method.

--- src/main/java/org/elasticsearch/plugin/elasticfence/AuthRestHandler.java
+++ src/main/java/org/elasticsearch/plugin/elasticfence/AuthRestHandler.java
@@ -26,124 +26,98 @@ public class AuthRestHandler extends BaseRestHandler {
     }
 
 	@Override
-	protected AuthRestChannelConsumer prepareRequest(RestRequest request, NodeClient client) throws IOException {
-		return new AuthRestChannelConsumer(request, client);
-	}
+	protected RestChannelConsumer prepareRequest(RestRequest request, NodeClient client) throws IOException {
+        String mode = request.param("mode");
+        if (mode == null) {
+            return channel -> channel.sendResponse(new BytesRestResponse(OK, "No Method"));
+        }
 
-	//alternative, look at RestFieldStatsAction.java in ES repo
+        UserDataBridge userDataBridge = new UserDataBridge(client);
+        if (!userDataBridge.isInitialized()) {
+            return channel -> channel.sendResponse(new BytesRestResponse(SERVICE_UNAVAILABLE, "http user auth initializing..."));
+        }
+        if ("list".equals(mode)) {
+            try {
+                String userListJSON = userDataBridge.listUser();
+                return channel -> channel.sendResponse(new BytesRestResponse(OK, userListJSON));
+            } catch (Exception ex) {
+                EFLogger.error("failed to create index: ", ex);
+                return channel -> channel.sendResponse(new BytesRestResponse(OK, "failed to list all users"));
+            }
+        }
+        if ("adduser".equals(mode)) {
+            String userName = request.param("username");
+            String password = request.param("password");
+            try {
+                boolean res = userDataBridge.createUser(userName, password);
+                if (res) {
+                    return channel -> channel.sendResponse(new BytesRestResponse(OK, "User created : " + userName));
+                } else {
+                    return channel -> channel.sendResponse(new BytesRestResponse(CONFLICT, "User already exists : " + userName));
+                }
+            } catch (Exception ex) {
+                EFLogger.error("failed to create index: ", ex);
+                return channel -> channel.sendResponse(new BytesRestResponse(OK, "failed to create index : " + userName));
+            }
+        }
+        if ("addindex".equals(mode)) {
+            String userName  = request.param("username");
+            String indexName = request.param("index");
 
-	private class AuthRestChannelConsumer implements BaseRestHandler.RestChannelConsumer {
-		NodeClient client;
-		RestRequest request;
-		AuthRestChannelConsumer(RestRequest request, NodeClient client) {
-			this.request = request;
-			this.client = client;
-		}
+            try {
+                boolean res = userDataBridge.addAuthIndex(userName, indexName);
+                if (res) {
+                    return channel -> channel.sendResponse(new BytesRestResponse(OK, "added auth index: " + userName));
+                } else {
+                    return channel -> channel.sendResponse(new BytesRestResponse(OK, "failed to add auth index: " + userName));
+                }
+            } catch (Exception ex) {
+                EFLogger.error("failed to add auth index: ", ex);
+                return channel -> channel.sendResponse(new BytesRestResponse(OK, "failed to add auth index : " + userName));
+            }
+        }
 
-		@Override
-		public void accept(RestChannel channel) {
-			String mode = request.param("mode");
-			if (mode == null) {
-				channel.sendResponse(new BytesRestResponse(OK, "No Method"));
-				return ;
-			}
+        if ("updateindex".equals(mode)) {
+            String userName  = request.param("username");
+            String indexName = request.param("index");
 
-			UserDataBridge userDataBridge = new UserDataBridge(client);
-			if (!userDataBridge.isInitialized()) {
-				channel.sendResponse(new BytesRestResponse(SERVICE_UNAVAILABLE, "http user auth initializing..."));
-				return ;
-			}
-			if ("list".equals(mode)) {
-				try {
-					String userListJSON = userDataBridge.listUser();
-					channel.sendResponse(new BytesRestResponse(OK, userListJSON));
-				} catch (Exception ex) {
-					EFLogger.error("failed to create index: ", ex);
-					channel.sendResponse(new BytesRestResponse(OK, "failed to list all users"));
-				}
-				return ;
-			}
-			if ("adduser".equals(mode)) {
-				String userName = request.param("username");
-				String password = request.param("password");
-				try {
-					boolean res = userDataBridge.createUser(userName, password);
-					if (res) {
-						channel.sendResponse(new BytesRestResponse(OK, "User created : " + userName));
-					} else {
-						channel.sendResponse(new BytesRestResponse(CONFLICT, "User already exists : " + userName));
-					}
-					return ;
-				} catch (Exception ex) {
-					EFLogger.error("failed to create index: ", ex);
-					channel.sendResponse(new BytesRestResponse(OK, "failed to create index : " + userName));
-					return ;
-				}
-			}
-			if ("addindex".equals(mode)) {
-				String userName  = request.param("username");
-				String indexName = request.param("index");
+            try {
+                boolean res = userDataBridge.updateAuthIndex(userName, indexName);
+                if (res) {
+                    return channel -> channel.sendResponse(new BytesRestResponse(OK, "added auth index: " + userName));
+                } else {
+                    return channel -> channel.sendResponse(new BytesRestResponse(OK, "failed to add auth index: " + userName));
+                }
+            } catch (Exception ex) {
+                EFLogger.error("failed to add auth index: ", ex);
+                return channel -> channel.sendResponse(new BytesRestResponse(OK, "failed to add auth index : " + userName));
+            }
+        }
 
-				try {
-					boolean res = userDataBridge.addAuthIndex(userName, indexName);
-					if (res) {
-						channel.sendResponse(new BytesRestResponse(OK, "added auth index: " + userName));
-					} else {
-						channel.sendResponse(new BytesRestResponse(OK, "failed to add auth index: " + userName));
-					}
-				} catch (Exception ex) {
-					EFLogger.error("failed to add auth index: ", ex);
-					channel.sendResponse(new BytesRestResponse(OK, "failed to add auth index : " + userName));
-					return ;
-				}
-				return;
-			}
+        if ("passwd".equals(mode)) {
+            String userName = request.param("username");
+            String oldPassword = request.param("old_password");
+            String newPassword = request.param("new_password");
 
-			if ("updateindex".equals(mode)) {
-				String userName  = request.param("username");
-				String indexName = request.param("index");
+            boolean res = userDataBridge.changePassword(userName, oldPassword, newPassword);
+            if (res) {
+                return channel -> channel.sendResponse(new BytesRestResponse(OK, "Password changed : " + userName));
+            } else {
+                return channel -> channel.sendResponse(new BytesRestResponse(OK, "Failed to change Password: " + userName));
+            }
+        }
 
-				try {
-					boolean res = userDataBridge.updateAuthIndex(userName, indexName);
-					if (res) {
-						channel.sendResponse(new BytesRestResponse(OK, "added auth index: " + userName));
-					} else {
-						channel.sendResponse(new BytesRestResponse(OK, "failed to add auth index: " + userName));
-					}
-				} catch (Exception ex) {
-					EFLogger.error("failed to add auth index: ", ex);
-					channel.sendResponse(new BytesRestResponse(OK, "failed to add auth index : " + userName));
-					return ;
-				}
-				return;
-			}
+        if ("deleteuser".equals(mode)) {
+            String userName  = request.param("username");
 
-			if ("passwd".equals(mode)) {
-				String userName = request.param("username");
-				String oldPassword = request.param("old_password");
-				String newPassword = request.param("new_password");
+            boolean res = userDataBridge.deleteUser(userName);
+            if (res) {
+                return channel -> channel.sendResponse(new BytesRestResponse(OK, "deleted user: " + userName));
+            } else {
+                return channel -> channel.sendResponse(new BytesRestResponse(OK, "failed to delete user: " + userName));
+            }
+        }
 
-				boolean res = userDataBridge.changePassword(userName, oldPassword, newPassword);
-				if (res) {
-					channel.sendResponse(new BytesRestResponse(OK, "Password changed : " + userName));
-				} else {
-					channel.sendResponse(new BytesRestResponse(OK, "Failed to change Password: " + userName));
-				}
-				return;
-			}
-
-			if ("deleteuser".equals(mode)) {
-				String userName  = request.param("username");
-
-				boolean res = userDataBridge.deleteUser(userName);
-				if (res) {
-					channel.sendResponse(new BytesRestResponse(OK, "deleted user: " + userName));
-				} else {
-					channel.sendResponse(new BytesRestResponse(OK, "failed to delete user: " + userName));
-				}
-				return;
-			}
-			channel.sendResponse(new BytesRestResponse(OK, "Failed"));
-		}
-	}
+        return channel -> channel.sendResponse(new BytesRestResponse(OK, "Failed"));
+    }
 }
